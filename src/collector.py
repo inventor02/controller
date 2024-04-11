7fb43b58b323ec02611f19186c49e694eb67dd88
George Peppard <me@gpeppard.com>
2024-04-11 16:12:26 +0100

    completed collector

---
+++ src/collector.py
@@ -0,0 +1,63 @@
+import datetime
+import serial
+
+class Collector:
+    def __init__(self, config):
+        self.config = config
+        self.port = '/dev/ttyAMA1'
+        self.serial = serial.Serial(self.port, 115200, timeout=None)
+        self.start_time = datetime.datetime.now()
+
+    def collect(self):
+        line = self.serial.readline()
+        if line == '':
+            return None
+        
+        data = line.split(':')
+        if len(data) != 6:
+            return None
+        
+        try:
+            for i in range(2):
+                data[i] = int(data[i])
+        except ValueError:
+            return None
+
+        try:
+            for i in range(2, 5):
+                data[i] = float(data[i])
+        except ValueError:
+            return None
+    
+        return {
+            'lat': int(data[0]),
+            'long': int(data[1]),
+            'roll': float(data[2]),
+            'pitch': float(data[3]),
+            'yaw': float(data[4]),
+            'gs': float(data[5])
+        }
+    
+    def commit_to_fs(self, data):
+        # get current date
+        now = datetime.datetime.now()
+        date = now.date().isoformat()
+        filename = '/var/log/data/' + date + '.csv'
+        line = now.isoformat() + ',' + data['lat'] + ',' + data['long'] + ',' + data['roll'] + ',' + data['pitch'] + ',' + data['yaw'] + ',' + data['gs'];
+        needs_header = file.exists(filename) == False
+        with open(filename, 'a') as file:
+            if needs_header:
+                file.write('time,lat,long,roll,pitch,yaw,gs\n')
+            file.write(data + '\n')
+
+    def run(self):
+        while True:
+            data = self.collect()
+            if data is not None:
+                print('got: ' + data)
+                # send over zmq
+
+if __name__ == '__main__':
+    print('[collector] "You look fun! Wanna play?"')
+    collector = Collector()
+    collector.run()
--- src/common/config.py
+++ src/common/config.py
@@ -1,3 +1,13 @@
-class Configuration {
-    
-}
+import yaml
+
+class Config:
+    def __init__(self):
+        self.config = None
+        self.load_config()
+
+    def load_config(self):
+        with open("config.yaml", "r") as file:
+            self.config = yaml.safe_load(file)
+
+    def get(self, key):
+        return self.config[key]
